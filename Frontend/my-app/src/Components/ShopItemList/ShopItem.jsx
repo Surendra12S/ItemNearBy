05685fb6665fcac626dbef62c67996da3e2129a5
Surendra12S <dasarisurendra0604@gmail.com>
2025-08-03 20:13:39 +0530

    add change

--- Frontend/my-app/src/Components/ShopItemList/ShopItem.jsx
+++ Frontend/my-app/src/Components/ShopItemList/ShopItem.jsx
@@ -30,7 +30,7 @@ function ShopItem() {
           <div key={item._id} className="item-card">
             <img
               className="itemImg"
-              src={`${baseUrl}/uploads/${item.image}`}
+              src={`${item.image}`}
               alt={item.name}
             />
             <h3>{item.name}</h3>
